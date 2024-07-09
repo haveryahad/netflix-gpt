5b5eb9ce40eff382fc199003878adf7cd8e2eee5
ahavery <haveryahad.dev@gmail.com>
2024-07-10 01:02:47 +0530

    Getting Stared with Layout and Login Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-5xl text-center">Hello World!</div>;
+  return (
+    <div>
+      <Body />
+    </div>
+  );
 }
 
 export default App;
